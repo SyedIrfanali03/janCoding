cf53a55b367fcc4940abbb2abc7bb153eb78d206
SyedIrfanali03 <greenstaskmay@gmail.com>
2023-01-24 12:36:35 +0530

    Jhon code added

--- src/org/jan/JanPro.java
+++ src/org/jan/JanPro.java
@@ -2,10 +2,14 @@ package org.jan;
 
 public class JanPro {
 	
-	
+	private void jhon() {
+		System.out.println("Jhon coding added");
+
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("main method");
+		
 		
 	}
 	
